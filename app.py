ecdd94e1312ea13d99a8fd9014fbb03b3eec8865
Kaz <qasemrana117@gmail.com>
2024-12-09 22:55:09 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Create main window
+app = tk.Tk()
+app.title("LoopBack")
+app.geometry("600x200")  # Set size to 600x200 pixels
+app.resizable(False, False)  # Disable resizing
+
+# Set background color to #252422
+app.configure(bg="#252422")
+
+# Run the application
+app.mainloop()
